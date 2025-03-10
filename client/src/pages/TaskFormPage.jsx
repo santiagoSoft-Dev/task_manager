7c80b6ec11c93af26abbaea3959db998613b11a4
santiagoSoft-Dev <santiago.mahecha.per@gmail.com>
2025-03-10 17:00:51 -0500

    Implementar TailwindCss

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -2,22 +2,37 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { createTask, deleteTask, updateTask, getTask } from "../api/tasks.api";
 import { useNavigate, useParams } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
 export function TasksFormPage() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-    setValue
+    setValue,
   } = useForm();
   const navigate = useNavigate();
   const params = useParams();
 
   const onSubmit = handleSubmit(async (data) => {
     if (params.id) {
-      await updateTask(params.id, data)
+      await updateTask(params.id, data);
+      toast.success("Tarea Actualizada", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
     } else {
       await createTask(data);
+      toast.success("Tarea Creada", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
     }
     navigate("/tasks");
   });
@@ -25,21 +40,22 @@ export function TasksFormPage() {
   useEffect(() => {
     async function loadTask() {
       if (params.id) {
-        const {data} = await getTask(params.id);
-        setValue('title', data.title)
-        setValue('description', data.description)
+        const { data } = await getTask(params.id);
+        setValue("title", data.title);
+        setValue("description", data.description);
       }
     }
     loadTask();
   }, []);
 
   return (
-    <div>
+    <div className="max-w-xl mx-auto">
       <form onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="title"
           {...register("title", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
         />
         {errors.title && <span>Title is required.</span>}
 
@@ -47,24 +63,37 @@ export function TasksFormPage() {
           rows="3"
           placeholder="Description"
           {...register("description", { required: true })}
+          className="bg-zinc-700 p-3 rounded-lg block w-full mb-3"
         ></textarea>
         {errors.description && <span>Description is required.</span>}
 
-        <button>Guardar</button>
+        <button className="bg-indigo-500 p-3 rounded-lg block w-full mt-3">
+          Guardar
+        </button>
       </form>
 
       {params.id && (
-        <button
-          onClick={async () => {
-            const accepted = window.confirm("¿Desea eliminar esta tarea?");
-            if (accepted) {
-              await deleteTask(params.id);
-              navigate("/tasks");
-            }
-          }}
-        >
-          Delete
-        </button>
+        <div className="flex justify-end">
+          <button
+            className="bg-red-500 p-3 rounded-lg w-48 mt-3"
+            onClick={async () => {
+              const accepted = window.confirm("¿Desea eliminar esta tarea?");
+              if (accepted) {
+                await deleteTask(params.id);
+                toast.success("Tarea Eliminada", {
+                  position: "bottom-right",
+                  style: {
+                    background: "#101010",
+                    color: "#fff",
+                  },
+                });
+                navigate("/tasks");
+              }
+            }}
+          >
+            Delete
+          </button>
+        </div>
       )}
     </div>
   );
